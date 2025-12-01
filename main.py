d03e7b561ae8d22d219fffb4f8bc19b46ffb9223
𝒩𝑜𝓏𝒾𝓁𝒶 <nozilarajabboyeva@gmail.com>
2025-12-01 14:57:28 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,49 @@
+#1-misol
+n = "mathematics"
+a = n[:4]
+print(a)
+
+#2-misol
+n = "development"
+a = n[:6]
+print(a)
+
+#3-misol
+n = "programmingLanguage"
+a = n[:8]
+print(a)
+
+#4-misol
+n = "abcdefjklm"
+a = n[::-1]
+print(a)
+
+#5-misol
+n = "0123456789"
+a = n[::3]
+print(a)
+
+#6-misol
+n = "HelloPythonWorld"
+a = n[5:11]
+print(a)
+
+#7-misol
+n = "IncredibleWork"
+a = n[2:8]
+print(a)
+
+#8-misol
+n = "ABCDEFGHIJK"
+a = n[3:8]
+print(a)
+
+#9-misol
+n = "superdeveloper"
+a = n[-5:]
+print(a)
+
+#10-misol
+n = "123456789"
+a = n[1::2]
+print(a)
